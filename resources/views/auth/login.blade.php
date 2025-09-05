84b55baf8dc64c7b55d0a3b43cd27dfb2d931f6f
Dhnisyhdla <58935810+Dhnzz@users.noreply.github.com>
2025-09-06 02:29:02 +0800

    Fix : App Logo
    Hapus beberapa gambar yang tidak diperlukan dan perbarui logo pada halaman login dan registrasi. Ganti tombol aksi menjadi lebih mencolok dengan warna primer. Perbarui tampilan sidebar untuk menyesuaikan ukuran favicon.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,8 +36,8 @@
                     <div class="col-md-8 col-lg-6 col-xxl-3">
                         <div class="card mb-0">
                             <div class="card-body">
-                                <a href="{{ url('/') }}" class="text-nowrap logo-img text-center d-block mb-5 w-100">
-                                    <img src="{{ asset('assets/front/img/logo.jpg') }}" width="180" alt="">
+                                <a href="{{ url('/') }}" class="text-nowrap logo-img text-center d-block mb-3 w-100">
+                                    <img src="{{ asset('assets/front/img/logo/sellora-vertical.png') }}" width="280" alt="">
                                 </a>
                                 <div class="position-relative text-center my-4">
                                     <p class="mb-0 fs-4 px-3 d-inline-block bg-white text-dark z-index-5 position-relative">
@@ -85,7 +85,7 @@
                                             @enderror
                                         </div>
                                     </div>
-                                    <button type="submit" class="btn btn-dark w-100 py-8 mb-2 rounded-2">Masuk</button>
+                                    <button type="submit" class="btn btn-primary w-100 py-8 mb-2 rounded-2">Masuk</button>
                                 </form>
                                 <a class="mt-2 text-center" style="width: 100%" href="{{ route('register') }}">Belum punya akun?</a>
                             </div>
