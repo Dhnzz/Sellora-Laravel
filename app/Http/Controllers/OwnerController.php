9f50bc29ea132379e6203aeb79b0bac22110d715
Dhnisyhdla <58935810+Dhnzz@users.noreply.github.com>
2025-09-10 18:41:32 +0800

    Refactor : OwnerController@topSales
    memperbaiki query untuk jumlah penjualan berhasi tiap sales

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -311,7 +311,7 @@ class OwnerController
     {
         $range = $request->input('range', 'weekly');
 
-        $query = SalesTransaction::query();
+        $query = SalesTransaction::query()->where('transaction_status', 'success');
         if ($range === 'weekly') {
             $query->whereBetween('invoice_date', [now()->subDays(6)->toDateString(), now()->toDateString()]);
         } elseif ($range === 'monthly') {
