3eb35d2a97599e4e12b159b75543d3fea3f69f8b
Dhnisyhdla <58935810+Dhnzz@users.noreply.github.com>
2025-09-10 17:47:47 +0800

    Update: Mengubah tahun copyright di footer dari 2023 Tethuna menjadi 2025 Sellora

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="py-6 px-6 text-center">
-    <p class="mb-0 fs-4">Copyright © 2023 Tethuna. All Rights Reserved.</p>
+    <p class="mb-0 fs-4">Copyright © 2025 Sellora. All Rights Reserved.</p>
 </div>
